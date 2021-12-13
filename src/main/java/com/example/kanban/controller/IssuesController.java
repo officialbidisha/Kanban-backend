e0bda6238bfb6fbb8c6b26bb70566d704c37cf1d
Bidisha Das <officialbidisha1@gmail.com>
2021-12-13 23:56:07 +0530

    Added status code

--- src/main/java/com/example/kanban/controller/IssuesController.java
+++ src/main/java/com/example/kanban/controller/IssuesController.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.kanban.model.Issues;
@@ -30,21 +33,30 @@ public class IssuesController {
 		issueRepository.save(Issue);
 		return Issue;
 	}
-	
+
+	@CrossOrigin(origins="*")
 	@RequestMapping(value = "/", method= RequestMethod.GET)
 	public List<Issues> getAllIssues(){
 		return issueRepository.findAll();
 	}
 	
+	@CrossOrigin(origins="*")
 	@RequestMapping(value = "/{id}", method= RequestMethod.GET)
 	public  Optional<Issues> getIssueById (@PathVariable("id") ObjectId _id) {
 		return issueRepository.findById(_id);
 	}
 	
+	@CrossOrigin(origins="*")
 	@RequestMapping(value= "/{id}", method= RequestMethod.POST)
-	public void modifyBookById(@PathVariable("id") ObjectId _id,@Validated @RequestBody Issues Issue) {
+	public HttpStatus modifyBookById(@PathVariable("id") ObjectId _id,@Validated @RequestBody Issues Issue) {
+		try {
 		Issue.setId(_id);
 		issueRepository.save(Issue);
+		return HttpStatus.CREATED;
+		}
+		catch(Exception e) {
+			return HttpStatus.EXPECTATION_FAILED;
+		}
 	}
 
 	@RequestMapping(value="/{id}",method = RequestMethod.DELETE)
